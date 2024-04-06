1a0f8065d65ef373c12deb7579fc61fa130e8e82
carlos1510 <carloscle1510@gmail.com>
2024-04-05 23:38:01 -0500

    agregar modificacion del fragment_list.xml

--- app/src/main/java/com/example/registropaciente/activity/ui/list/ListFragment.java
+++ app/src/main/java/com/example/registropaciente/activity/ui/list/ListFragment.java
@@ -1,4 +1,4 @@
-package com.example.registropaciente.activity.ui.slideshow;
+package com.example.registropaciente.activity.ui.list;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.registropaciente.databinding.FragmentSlideshowBinding;
+import com.example.registropaciente.databinding.FragmentListBinding;
 
-public class SlideshowFragment extends Fragment {
+public class ListFragment extends Fragment {
 
-    private FragmentSlideshowBinding binding;
+    private FragmentListBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+        ListViewModel listViewModel =
+                new ViewModelProvider(this).get(ListViewModel.class);
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        binding = FragmentListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.edtDniBuscar;
+        //listViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/registropaciente/activity/ui/list/ListViewModel.java
+++ app/src/main/java/com/example/registropaciente/activity/ui/list/ListViewModel.java
@@ -1,14 +1,14 @@
-package com.example.registropaciente.activity.ui.slideshow;
+package com.example.registropaciente.activity.ui.list;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class ListViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public ListViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is slideshow fragment");
     }
